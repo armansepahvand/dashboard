ba58503c4a350d9b639b94732c24090f1d3dcdbd
Arman Sepah Vand <v-asepahvand@microsoft.com>
2022-11-14 13:33:30 -0700

    initial setup of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { FieldSettings } from "@syncfusion/ej2-react-dropdowns";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import './App.css'
 
 const App = () => {
-  return <div>gfd</div>;
+  return <h1 className="underline text-3xl">App</h1>;
 };
 
 export default App;
